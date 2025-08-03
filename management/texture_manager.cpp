acd256db1e7d053753a0b23d0b7cf7a1521f00b2
meepedmeepo <meepedmeepo@gmail.com>
2025-08-03 19:48:33 +0000

    small changes

--- management/texture_manager.cpp
+++ management/texture_manager.cpp
@@ -1,8 +1,11 @@
 #include "texture_manager.h"
 #include <cstdio>
 
+//reminder of how to search a std::map
+//if(auto res = mTextures.find("foo"); res != mTextures.end()){
+//    printf("FOUND!");
+    //}
+
 void TextureManager::loadTexture(){
-    if(auto res = mTextures.find("cum"); res != mTextures.end()){
-        printf("FOUND!");
-    }
+
 }
